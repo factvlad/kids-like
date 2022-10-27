e889ed525e74572d42bf4fc3e6846ea3b7952331
Vladislav <fact1@ukr.net>
2022-10-27 20:56:34 +0300

    redux app

--- src/pages/authorization/AuthorizationPage.jsx
+++ src/pages/authorization/AuthorizationPage.jsx
@@ -1,26 +1,31 @@
+import { useDispatch } from "react-redux";
 import { useState } from "react";
 import FormLogIn from "components/form-login/FormLogIn";
+import { registerOperation, loginOperation } from "../../redux/auth/auth-operations"
 
 const AuthorizationPage = () => {
-    const [enterForm, setEnterForm] = useState('LogIn')
+  const [enterForm, setEnterForm] = useState('LogIn')
 
-    const changeEnterFetch = ({ target }) => {
-        const { name } = target
-        setEnterForm(name)
-    }
+  const changeEnterFetch = ({ target }) => {
+    const { name } = target
+    setEnterForm(name)
+  }
+  const dispatch = useDispatch()
 
-    const onSubmitForm = (data) => {
-        if (enterForm === 'LogIn') {
-            console.log(`login`);
-            console.log(data);
-            return
-        }
-        console.log(`register`)
-        console.log(data)
+  const onSubmitForm = (data) => {
+    if (enterForm === 'LogIn') {
+      console.log(`login`);
+      console.log(data);
+      dispatch(loginOperation(data))
+      return
     }
-    return (
-        <FormLogIn onSubmit={onSubmitForm} changeFetch={changeEnterFetch} />
-    );
+    console.log(`register`)
+    console.log(data)
+    dispatch(registerOperation(data))
+  }
+  return (
+    <FormLogIn onSubmit={ onSubmitForm } changeFetch={ changeEnterFetch } />
+  );
 }
 
 export default AuthorizationPage;
--- src/shared/hooks/useForm.jsx
+++ src/shared/hooks/useForm.jsx
@@ -1,24 +1,24 @@
 import { useState } from "react";
 
 const useForm = ({ initialState, onSubmit }) => {
-    const [state, setState] = useState({ ...initialState })
+  const [state, setState] = useState({ ...initialState })
 
-    const handleChange = ({ target }) => {
-        const { value, name, type, checked } = target
+  const handleChange = ({ target }) => {
+    const { value, name, type, checked } = target
 
-        const getValue = type === 'checkbox' ? checked : value
+    const getValue = type === 'checkbox' ? checked : value
 
-        setState(prevState => ({
-            ...prevState,
-            [name]: getValue
-        }))
-    }
-    const handleSubmit = e => {
-        e.preventDefault()
-        onSubmit({ ...state })
-        setState({ ...initialState })
-    }
-    return { state, setState, handleChange, handleSubmit };
+    setState(prevState => ({
+      ...prevState,
+      [name]: getValue
+    }))
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+    onSubmit({ ...state })
+    setState({ ...initialState })
+  }
+  return { state, setState, handleChange, handleSubmit };
 }
 
 export default useForm;

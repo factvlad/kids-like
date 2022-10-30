41d5314a2cbd7f2ccf584daaee0fa0add508bb35
LeonidGHA <104501564+LeonidGHA@users.noreply.github.com>
2022-10-30 10:59:52 +0200

    add logOut

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutOperation } from '../../redux/auth/auth-operations'
 
 const Header = () => {
+    const dispatch = useDispatch()
     return (
         <header>
             <div className='container'>
@@ -12,8 +15,8 @@ const Header = () => {
                         <NavLink to='/awards'>Awards</NavLink>
                         <NavLink to='/contacts'>Contacts</NavLink>
                         <NavLink to='/planning'>Planning</NavLink>
-
                     </nav>
+                    <button type="button" onClick={() => dispatch(logoutOperation())}>LogOut</button>
                 </div>
             </div>
         </header>
